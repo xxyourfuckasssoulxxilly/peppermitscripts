7ef47992ee176e2b77895a771cacf96d025ab042
xxyourzetaoulxxilly <delucavale12@gmail.com>
2026-06-18 15:55:17 +0200

    Update spawner.lua

--- spawner.lua
+++ spawner.lua
@@ -816,7 +816,7 @@ for _, wrapper in pairs(ClientData.get("pet_char_wrappers")) do
 end
 
 local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "blueprint_lua"
+screenGui.Name = "zetascripts"
 screenGui.ResetOnSpawn = false
 screenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 screenGui.IgnoreGuiInset = false
@@ -861,7 +861,7 @@ local title = Instance.new("TextLabel")
 title.Size = UDim2.new(1, 0, 0, 20)
 title.Position = UDim2.new(0, 0, 0, 4)
 title.BackgroundTransparency = 1
-title.Text = "Zeta scripts"
+title.Text = "ZetaScripts(last4zeta on tt)"
 title.Font = Enum.Font.GothamBold
 title.TextSize = 12
 title.TextColor3 = Color3.fromRGB(235, 240, 255)
